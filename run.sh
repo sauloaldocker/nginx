a5f8b64873ec45702ad148f4ddd0d454939e8629
Saulo Alves <sauloal@gmail.com>
2014-09-11 00:03:27 +0200

    right data folder

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 docker stop nginx; docker rm nginx
 
+SRC=$HOME/data/docker/nginx
+
 FIL=""
 if [[ -f "nginx.conf" ]]; then
-	FIL="-v $PWD/nginx.conf:/etc/nginx.conf:ro"
+	FIL="-v $SRC/nginx.conf:/etc/nginx.conf:ro"
 fi
 
-docker run --name nginx -v $PWD/data:/usr/local/nginx/html:ro $FIL -d -p 127.0.1.1:80:80 nginx
+docker run --name nginx -v $SRC/data:/usr/local/nginx/html:ro $FIL -d -p 127.0.1.1:800:80 nginx
