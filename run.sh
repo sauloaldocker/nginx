dacf5f4b2fec5efd417c0ce3d22b9e18ffb03cfe
Saulo Alves <sauloal@gmail.com>
2014-09-12 23:32:14 +0200

    separation

--- run.sh
+++ run.sh
@@ -3,8 +3,15 @@ docker stop nginx; docker rm nginx
 SRC=$HOME/data/docker/nginx
 
 FIL=""
-if [[ -f "nginx.conf" ]]; then
+if [[ -f "$SRC/nginx.conf" ]]; then
 	FIL="-v $SRC/nginx.conf:/etc/nginx.conf:ro"
+	FIL="-v $SRC/sites-enabled:/etc/nginx/sites-enabled:ro $FIL"
+	FIL="-v $SRC/data:/usr/local/nginx/html:ro $FIL"
 fi
 
-docker run --name nginx -v $SRC/data:/usr/local/nginx/html:ro $FIL -d -p 127.0.1.1:800:80 nginx
+LNK=""
+#LNK="$LNK --link dockerui:dockerui"
+#LNK="$LNK --link dockerui:dockerui"
+
+#docker run -d --name nginx --net host $LNK -p 127.0.1.1:80:80 nginx
+docker run -d --name nginx --net host $LNK $FIL nginx
